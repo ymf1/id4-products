89397b3d543d0124c9ed4c7170c1ec1908a51175
Erwin van der Valk <erwin@vandervalk.pro>
2025-01-08 16:24:45 +0100

    build works

--- bff/samples/Hosts.AppHost/Program.cs
+++ bff/samples/Hosts.AppHost/Program.cs
@@ -3,36 +3,33 @@ var builder = DistributedApplication.CreateBuilder(args);
 var idServer = builder.AddProject<Projects.IdentityServer>("identity-server");
 
 var api = builder.AddProject<Projects.Api>("api");
+var isolatedApi = builder.AddProject<Projects.Api_Isolated>("api-isolated");
 
 builder.AddProject<Projects.Bff>("bff")
     .WithExternalHttpEndpoints()
     .WithReference(idServer)
+    .WithReference(isolatedApi)
     .WithReference(api);
 
 builder.AddProject<Projects.Bff_EF>("bff-ef")
     .WithExternalHttpEndpoints()
     .WithReference(idServer)
+    .WithReference(isolatedApi)
     .WithReference(api);
 
 builder.AddProject<Projects.WebAssembly>("bff-webassembly-per-component")
     .WithExternalHttpEndpoints()
     .WithReference(idServer)
+    .WithReference(isolatedApi)
     .WithReference(api);
 
-//builder.AddProject<Projects.WebAssembly_Client>("bff-webassembly-per-component-client")
-//    .WithExternalHttpEndpoints()
-//    .WithReference(idServer)
-//    .WithReference(api);
+
 
 builder.AddProject<Projects.PerComponent>("bff-blazor-per-component")
     .WithExternalHttpEndpoints()
     .WithReference(idServer)
+    .WithReference(isolatedApi)
     .WithReference(api);
-
-//builder.AddProject<Projects.PerComponent_Client>("bff-blazor-per-component-client")
-//    .WithExternalHttpEndpoints()
-//    .WithReference(idServer)
-//    .WithReference(api);
 
 var apiDPop = builder.AddProject<Projects.Api_DPoP>("api-dpop");
 

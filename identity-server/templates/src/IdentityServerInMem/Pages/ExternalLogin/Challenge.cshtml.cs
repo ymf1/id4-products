f16cb4f06963a273385f08592ac6e78e4daf0188
Erwin van der Valk <erwin@vandervalk.pro>
2025-03-07 10:48:44 +0100

    remove header from templates (#1844)

--- identity-server/templates/src/IdentityServerInMem/Pages/ExternalLogin/Challenge.cshtml.cs
+++ identity-server/templates/src/IdentityServerInMem/Pages/ExternalLogin/Challenge.cshtml.cs
@@ -1,6 +1,3 @@
-// Copyright (c) Duende Software. All rights reserved.
-// See LICENSE in the project root for license information.
-
 using Duende.IdentityServer.Services;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
--- identity-server/templates/src/IdentityServerInMem/Program.cs
+++ identity-server/templates/src/IdentityServerInMem/Program.cs
@@ -1,6 +1,3 @@
-// Copyright (c) Duende Software. All rights reserved.
-// See LICENSE in the project root for license information.
-
 using System.Globalization;
 using System.Text;
 using Duende.IdentityServer.Licensing;

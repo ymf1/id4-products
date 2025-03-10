af23bce6f52e930ee0e7ca225586e753554426a9
Joe DeCock <josephdecock@gmail.com>
2025-03-10 11:53:03 -0500

    Fix up an accidentally reverted Shouldly change

--- identity-server/test/IdentityServer.UnitTests/Configuration/DependencyInjection/PostConfigureApplicationCookieTicketStoreTests.cs
+++ identity-server/test/IdentityServer.UnitTests/Configuration/DependencyInjection/PostConfigureApplicationCookieTicketStoreTests.cs
@@ -1,11 +1,12 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
 using Duende.IdentityServer.Configuration;
 using Duende.IdentityServer.Licensing.V2;
-using FluentAssertions;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using UnitTests.Common;
-using Xunit;
 
 namespace UnitTests.Configuration.DependencyInjection;
 
@@ -22,21 +23,21 @@ public class PostConfigureApplicationCookieTicketStoreTests
             sp.AddSingleton<LicenseAccessor>();
             sp.AddSingleton<LicenseUsageTracker>();
         });
-        
+
         // The mock http context accessor has a convenient HttpContext, but
         // initially we simulate not having it by stashing it away and setting
         // the accessor's context to null.
-        var savedContext = httpContextAccessor.HttpContext; 
+        var savedContext = httpContextAccessor.HttpContext;
         httpContextAccessor.HttpContext = null;
 
         var sut = new PostConfigureApplicationCookieTicketStore(
             httpContextAccessor,
             new IdentityServerOptions
             {
-                Authentication = new AuthenticationOptions 
+                Authentication = new AuthenticationOptions
                 {
                     // This is needed so that we operate on the correct scheme
-                    CookieAuthenticationScheme = CookieAuthenticationDefaults.AuthenticationScheme 
+                    CookieAuthenticationScheme = CookieAuthenticationDefaults.AuthenticationScheme
                 }
             },
             Options.Create<Microsoft.AspNetCore.Authentication.AuthenticationOptions>(new()),
@@ -48,6 +49,6 @@ public class PostConfigureApplicationCookieTicketStoreTests
         var cookieOpts = new CookieAuthenticationOptions();
         sut.PostConfigure(CookieAuthenticationDefaults.AuthenticationScheme, cookieOpts);
 
-        cookieOpts.SessionStore.Should().BeOfType<TicketStoreShim>();
+        cookieOpts.SessionStore.ShouldBeOfType<TicketStoreShim>();
     }
 }

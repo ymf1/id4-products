3fca806aa7bd363c5123134e9f05c61f0d36e7bb
Stu Frankish <stuart.frankish@duendesoftware.com>
2025-03-12 14:49:31 +0000

    Removed using that snuck in.

--- identity-server/hosts/AspNetIdentity/Pages/Account/Login/Index.cshtml.cs
+++ identity-server/hosts/AspNetIdentity/Pages/Account/Login/Index.cshtml.cs
@@ -12,7 +12,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace IdentityServerHost.Pages.Login;
 

66c5e02b310d1e19c2588ce29313082d8ff8fd6b
Stu Frankish <stuart.frankish@duendesoftware.com>
2025-03-12 14:46:37 +0000

    Validate that "RememberMe" is allowed.
    Ensure that Remember Me is allowed when passing it to the sign-in manager.

--- identity-server/hosts/AspNetIdentity/Pages/Account/Login/Index.cshtml.cs
+++ identity-server/hosts/AspNetIdentity/Pages/Account/Login/Index.cshtml.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace IdentityServerHost.Pages.Login;
 
@@ -97,7 +98,10 @@ public class Index : PageModel
 
         if (ModelState.IsValid)
         {
-            var result = await _signInManager.PasswordSignInAsync(Input.Username!, Input.Password!, Input.RememberLogin, lockoutOnFailure: true);
+            // Only remember login if allowed
+            var rememberLogin = View.AllowRememberLogin && Input.RememberLogin;
+
+            var result = await _signInManager.PasswordSignInAsync(Input.Username!, Input.Password!, isPersistent: rememberLogin, lockoutOnFailure: true);
             if (result.Succeeded)
             {
                 var user = await _userManager.FindByNameAsync(Input.Username!);

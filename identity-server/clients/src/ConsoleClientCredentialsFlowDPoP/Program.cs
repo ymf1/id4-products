f5b45c18cbe644ee12c66da4d1ec6a8d975c4c47
Joe DeCock <josephdecock@gmail.com>
2025-03-21 20:00:53 -0500

    Refactor DPoP test client
    The test client wasn't using service discovery, because when it created an HttpClient, it was creating a brand new service collection, so the AddServiceDefaults call didn't interact with it.

--- identity-server/clients/src/ConsoleClientCredentialsFlowDPoP/Program.cs
+++ identity-server/clients/src/ConsoleClientCredentialsFlowDPoP/Program.cs
@@ -14,16 +14,31 @@ var builder = Host.CreateApplicationBuilder(args);
 // Add ServiceDefaults from Aspire
 builder.AddServiceDefaults();
 
-var authority = builder.Configuration["is-host"];
-var simpleApi = builder.Configuration["simple-api"];
-
 var discoClient = new HttpClient();
-
+var authority = builder.Configuration["is-host"];
 var disco = await discoClient.GetDiscoveryDocumentAsync(authority);
 if (disco.IsError) throw new Exception(disco.Error);
 
-var jwkJson = CreateDPoPKey();
-var client = GetHttpClient(disco.TokenEndpoint, jwkJson);
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddClientCredentialsTokenManagement()
+    .AddClient("client", client =>
+    {
+        client.TokenEndpoint = disco.TokenEndpoint;
+        client.ClientId = "client";
+        client.ClientSecret = "secret";
+        client.DPoPJsonWebKey =  CreateDPoPKey();
+    });
+
+builder.Services.AddClientCredentialsHttpClient("test", "client", config =>
+    {
+        config.BaseAddress = new Uri("https://dpop-api");
+    })
+    .AddServiceDiscovery();
+
+var host = builder.Build();
+
+var client = host.Services.GetRequiredService<IHttpClientFactory>().CreateClient("test");
+
 
 var response = await client.GetStringAsync("identity");
 
@@ -33,28 +48,6 @@ Console.WriteLine(response.PrettyPrintJson());
 // Graceful shutdown
 Environment.Exit(0);
 
-HttpClient GetHttpClient(string tokenEndpoint, string jwk)
-{
-    var services = new ServiceCollection();
-    services.AddDistributedMemoryCache();
-    services.AddClientCredentialsTokenManagement()
-        .AddClient("client", client =>
-        {
-            client.TokenEndpoint = tokenEndpoint;
-            client.ClientId = "client";
-            client.ClientSecret = "secret";
-            client.DPoPJsonWebKey = jwk;
-        });
-
-    services.AddClientCredentialsHttpClient("test", "client", config =>
-    {
-        config.BaseAddress = new Uri(simpleApi);
-    });
-
-    var provider = services.BuildServiceProvider();
-    var client = provider.GetRequiredService<IHttpClientFactory>().CreateClient("test");
-    return client;
-}
 
 static string CreateDPoPKey()
 {

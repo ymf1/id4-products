0b16dac8f12c84a4984e598349c7357daf21c26f
Erwin van der Valk <erwin@vandervalk.pro>
2025-04-02 11:20:43 +0200

    Add public api tests (#1934)

---
+++ bff/test/Bff.Tests/PublicApiVerificationTests.cs
@@ -0,0 +1,76 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+using Duende.Bff.Blazor;
+using Duende.Bff.Blazor.Client;
+using Duende.Bff.EntityFramework;
+using Duende.Bff.Yarp;
+using PublicApiGenerator;
+
+namespace Bff.Tests;
+
+public class PublicApiVerificationTests
+{
+
+    [Fact]
+    public async Task VerifyPublicApi_Bff()
+    {
+        var apiGeneratorOptions = new ApiGeneratorOptions
+        {
+            IncludeAssemblyAttributes = false
+        };
+        var publicApi = typeof(BffBuilder).Assembly.GeneratePublicApi(apiGeneratorOptions);
+        var settings = new VerifySettings();
+        await Verify(publicApi, settings);
+    }
+
+    [Fact]
+    public async Task VerifyPublicApi_Bff_Yarp()
+    {
+        var apiGeneratorOptions = new ApiGeneratorOptions
+        {
+            IncludeAssemblyAttributes = false
+        };
+        var publicApi = typeof(AccessTokenRequestTransform).Assembly.GeneratePublicApi(apiGeneratorOptions);
+        var settings = new VerifySettings();
+        await Verify(publicApi, settings);
+    }
+
+    [Fact]
+    public async Task VerifyPublicApi_Bff_EntityFramework()
+    {
+        var apiGeneratorOptions = new ApiGeneratorOptions
+        {
+            IncludeAssemblyAttributes = false
+        };
+        var publicApi = typeof(ISessionDbContext).Assembly.GeneratePublicApi(apiGeneratorOptions);
+        var settings = new VerifySettings();
+        await Verify(publicApi, settings);
+    }
+
+    [Fact]
+    public async Task VerifyPublicApi_Bff_Blazor()
+    {
+        var apiGeneratorOptions = new ApiGeneratorOptions
+        {
+            IncludeAssemblyAttributes = false
+        };
+        var publicApi = typeof(BffBlazorServerOptions).Assembly.GeneratePublicApi(apiGeneratorOptions);
+        var settings = new VerifySettings();
+        await Verify(publicApi, settings);
+    }
+
+
+    [Fact]
+    public async Task VerifyPublicApi_Bff_Blazor_Client()
+    {
+        var apiGeneratorOptions = new ApiGeneratorOptions
+        {
+            IncludeAssemblyAttributes = false
+        };
+        var publicApi = typeof(AntiforgeryHandler).Assembly.GeneratePublicApi(apiGeneratorOptions);
+        var settings = new VerifySettings();
+        await Verify(publicApi, settings);
+    }
+
+}

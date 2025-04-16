DuendeSoftware/products

Added ability to request a JWT response from the token introspection endpoint

identity-server/src/IdentityServer/Endpoints/Results/IntrospectionResult.cs
@@ -2,8 +2,11 @@
 // See LICENSE in the project root for license information.
 
 
+using System.Security.Claims;
 using Duende.IdentityServer.Extensions;
 using Duende.IdentityServer.Hosting;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
 using Microsoft.AspNetCore.Http;
 
 namespace Duende.IdentityServer.Endpoints.Results;
@@ -23,22 +26,70 @@ public class IntrospectionResult : EndpointResult<IntrospectionResult>
     public Dictionary<string, object> Entries { get; }
 
     /// <summary>
+    /// Gets the name of the caller.
+    /// </summary>
+    /// <value>
+    /// Identifier of the request caller.
+    /// </value>
+    public string CallerName { get; }
+
+    /// <summary>
+    /// Gets if JWT response was requested.
+    /// </summary>
+    /// <value>
+    /// True if JWT response was requested.
+    /// </value>
+    public bool JwtResponseWasRequested { get; }
+
+    /// <summary>
     /// Initializes a new instance of the <see cref="IntrospectionResult"/> class.
     /// </summary>
     /// <param name="entries">The result.</param>
     /// <exception cref="System.ArgumentNullException">result</exception>
-    public IntrospectionResult(Dictionary<string, object> entries)
+    public IntrospectionResult(Dictionary<string, object> entries) : this(entries, null, false)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IntrospectionResult"/> class.
+    /// </summary>
+    /// <param name="entries">The result.</param>
+    /// <param name="callerName">The identifier of the party making the introspection request.</param>
+    /// <param name="jwtResponseWasRequested">If a JWT response was requested.</param>
+    /// <exception cref="System.ArgumentNullException">result</exception>
+    public IntrospectionResult(Dictionary<string, object> entries, string callerName, bool jwtResponseWasRequested)
     {
         Entries = entries ?? throw new ArgumentNullException(nameof(entries));
+        CallerName = callerName;
+        JwtResponseWasRequested = jwtResponseWasRequested;
     }
 }
 
-internal class IntrospectionHttpWriter : IHttpResponseWriter<IntrospectionResult>
+internal class IntrospectionHttpWriter(IIssuerNameService issuerNameService, ITokenCreationService tokenCreationService)
+    : IHttpResponseWriter<IntrospectionResult>
 {
-    public Task WriteHttpResponse(IntrospectionResult result, HttpContext context)
+    public async Task WriteHttpResponse(IntrospectionResult result, HttpContext context)
     {
         context.Response.SetNoCache();
 
-        return context.Response.WriteJsonAsync(result.Entries);
+        if (result.JwtResponseWasRequested)
+        {
+            context.Response.Headers.ContentType = $"application/{IdentityServerConstants.TokenTypes.IntrospectionResponseToken}";
+            var token = new Token
+            {
+                Type = IdentityServerConstants.TokenTypes.IntrospectionResponseToken,
+                Issuer = await issuerNameService.GetCurrentAsync(),
+                Audiences = [result.CallerName],
+                CreationTime = DateTime.UtcNow,
+                Claims = [new Claim("token_introspection", ObjectSerializer.ToString(result.Entries), IdentityServerConstants.ClaimValueTypes.Json)]
+            };
+            var jwt = await tokenCreationService.CreateTokenAsync(token);
+
+            await context.Response.WriteAsync(jwt);
+        }
+        else
+        {
+            await context.Response.WriteJsonAsync(result.Entries);
+        }
     }
 }

identity-server/src/IdentityServer/Extensions/TokenExtensions.cs
@@ -111,6 +111,13 @@ public static class TokenExtensions
                 }
             }
 
+            if (token.Type == IdentityServerConstants.TokenTypes.IntrospectionResponseToken)
+            {
+                payload.Remove(JwtClaimTypes.Expiration);
+                payload.Remove(JwtClaimTypes.NotBefore);
+                payload.Remove(JwtClaimTypes.Subject);
+            }
+
             return payload;
         }
         catch (Exception ex)

ymf1/id4-products

identity-server/src/IdentityServer/Endpoints/Results/IntrospectionResult.cs
@@ -2,8 +2,12 @@
 // See LICENSE in the project root for license information.
 
 
+using System.Security.Claims;
+using Duende.IdentityModel;
 using Duende.IdentityServer.Extensions;
 using Duende.IdentityServer.Hosting;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
 using Microsoft.AspNetCore.Http;
 
 namespace Duende.IdentityServer.Endpoints.Results;
@@ -23,19 +27,70 @@ public class IntrospectionResult : EndpointResult<IntrospectionResult>
     public Dictionary<string, object> Entries { get; }
 
     /// <summary>
+    /// Gets the name of the caller.
+    /// </summary>
+    /// <value>
+    /// Identifier of the request caller.
+    /// </value>
+    public string CallerName { get; }
+
+    /// <summary>
+    /// Gets if JWT response was requested.
+    /// </summary>
+    /// <value>
+    /// True if JWT response was requested.
+    /// </value>
+    public bool JwtResponseWasRequested { get; }
+
+    /// <summary>
     /// Initializes a new instance of the <see cref="IntrospectionResult"/> class.
     /// </summary>
     /// <param name="entries">The result.</param>
     /// <exception cref="System.ArgumentNullException">result</exception>
-    public IntrospectionResult(Dictionary<string, object> entries) => Entries = entries ?? throw new ArgumentNullException(nameof(entries));
+    public IntrospectionResult(Dictionary<string, object> entries) : this(entries, null, false)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IntrospectionResult"/> class.
+    /// </summary>
+    /// <param name="entries">The result.</param>
+    /// <param name="callerName">The identifier of the party making the introspection request.</param>
+    /// <param name="jwtResponseWasRequested">If a JWT response was requested.</param>
+    /// <exception cref="System.ArgumentNullException">result</exception>
+    public IntrospectionResult(Dictionary<string, object> entries, string callerName, bool jwtResponseWasRequested)
+    {
+        Entries = entries ?? throw new ArgumentNullException(nameof(entries));
+        CallerName = callerName;
+        JwtResponseWasRequested = jwtResponseWasRequested;
+    }
 }
 
-internal class IntrospectionHttpWriter : IHttpResponseWriter<IntrospectionResult>
+internal class IntrospectionHttpWriter(IIssuerNameService issuerNameService, ITokenCreationService tokenCreationService)
+    : IHttpResponseWriter<IntrospectionResult>
 {
-    public Task WriteHttpResponse(IntrospectionResult result, HttpContext context)
+    public async Task WriteHttpResponse(IntrospectionResult result, HttpContext context)
     {
         context.Response.SetNoCache();
 
-        return context.Response.WriteJsonAsync(result.Entries);
+        if (result.JwtResponseWasRequested)
+        {
+            context.Response.Headers.ContentType = $"application/{JwtClaimTypes.JwtTypes.IntrospectionJwtResponse}";
+            var token = new Token
+            {
+                Type = JwtClaimTypes.JwtTypes.IntrospectionJwtResponse,
+                Issuer = await issuerNameService.GetCurrentAsync(),
+                Audiences = [result.CallerName],
+                CreationTime = DateTime.UtcNow,
+                Claims = [new Claim("token_introspection", ObjectSerializer.ToString(result.Entries), IdentityServerConstants.ClaimValueTypes.Json)]
+            };
+            var jwt = await tokenCreationService.CreateTokenAsync(token);
+
+            await context.Response.WriteAsync(jwt);
+        }
+        else
+        {
+            await context.Response.WriteJsonAsync(result.Entries);
+        }
     }
 }

identity-server/src/IdentityServer/Extensions/TokenExtensions.cs
@@ -111,6 +111,13 @@ public static class TokenExtensions
                 }
             }
 
+            if (token.Type == JwtClaimTypes.JwtTypes.IntrospectionJwtResponse)
+            {
+                payload.Remove(JwtClaimTypes.Expiration);
+                payload.Remove(JwtClaimTypes.NotBefore);
+                payload.Remove(JwtClaimTypes.Subject);
+            }
+
             return payload;
         }
         catch (Exception ex)